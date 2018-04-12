f42f6c6c04b8bd7eb4c16a00ca1d44931f33b892
Nathan Clark Baumgartner <nbaumg@hotmail.com>
2018-04-12 14:37:12 -0500

    added message service and component

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,14 +3,19 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/Of';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
+  constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
+    //TODO: send the message after fetching the heroes
+    this.messageService.add('Hero Service: fetched heroes');
     return of(HEROES);
   }
 
-  constructor() { }
+  
 
 }
